4a11c78bfee9505ccdf7b6f59795399af8486cc2
Leo <leogp6@gmail.com>
2023-10-22 20:05:28 -0300

    Adicionando as primeiras funcionalidades

---
+++ carteiravirtual.js
@@ -0,0 +1,37 @@
+angular.module('carteiraVirtual', []);
+angular.module('carteiraVirtual').controller('carteiraVirtualCtrl', function($scope){
+
+    $scope.app = 'Carteira Virtual';
+
+    $scope.totais = {caixa: 0, divida: 0};
+
+    $scope.tipo = true;
+
+    $scope.registros = [];
+
+    $scope.mudaTipo = function() {
+        $scope.tipo = !$scope.tipo;
+    };
+
+    $scope.incrementaTotais = function(registro){
+        if (!$scope.tipo) {
+            $scope.totais.divida += registro;
+            return;
+        }
+        $scope.totais.caixa += registro;
+    };
+
+    $scope.RegistraTabela = function(registro){
+
+    };
+
+    $scope.Adicionar = function(registro){
+        $scope.RegistraTabela(registro);
+        $scope.incrementaTotais(registro);
+        delete $scope.registro
+    };
+
+    $scope.verificaRegistro = function(registo) {
+
+    };
+});
