fc3fc42bc260853a327eb64bbe0a7eec5b693de3
Leo <leogp6@gmail.com>
2023-10-24 23:14:02 -0300

    Adicionando os botões de editar e deletar

--- carteiravirtual.js
+++ carteiravirtual.js
@@ -13,21 +13,21 @@ angular.module('carteiraVirtual').controller('carteiraVirtualCtrl', function($sc
         $scope.tipo = !$scope.tipo;
     };
 
-    $scope.incrementaTotais = function(valor){
-        if (!$scope.tipo) {
+    $scope.incrementaTotais = function(valor, tipo){
+        if (!tipo) {
             $scope.totais.divida += valor;
             return;
         }
         $scope.totais.caixa += valor;
     };
 
-    $scope.RegistraTabela = function(valor, arrayRegistros){
-        arrayRegistros.push({data: new Date(), tipo: $scope.tipo, valor: valor});
+    $scope.RegistraTabela = function(valor, tipo, arrayRegistros){
+        arrayRegistros.push({data: new Date(), tipo: tipo, valor: valor});
     };
 
-    $scope.Adicionar = function(valor){
-        $scope.RegistraTabela(valor, $scope.registros);
-        $scope.incrementaTotais(valor);
+    $scope.Adicionar = function(valor, tipo, registros){
+        $scope.RegistraTabela(valor, tipo, registros);
+        $scope.incrementaTotais(valor, tipo);
         delete $scope.valor
     };
 
@@ -36,5 +36,32 @@ angular.module('carteiraVirtual').controller('carteiraVirtualCtrl', function($sc
             return false;
         };
         return true;
+    };
+
+    $scope.deletaRegistros = function(registros) {
+        let nao_selecionados = [];
+        
+        for (let registro of registros){
+            if (registro.selecionado) {
+                $scope.incrementaTotais(-1*registro.valor, registro.tipo);
+            }
+            else {
+                nao_selecionados.push(registro);
+            };
+        };
+
+        $scope.registros = nao_selecionados;
+    };
+
+    $scope.verificaEditar = function(registros) {
+        return registros.filter(function(registro){
+            return registro.selecionado;
+        }).length == 1;
+    };
+
+    $scope.verificaDeletar = function(registros) {
+        return registros.some(function(registro){
+            return registro.selecionado;
+        });
     };
 });
